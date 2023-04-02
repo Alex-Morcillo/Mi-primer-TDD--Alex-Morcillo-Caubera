1ea099a71c59eb28729673fa722c63372b3f8b64
Alex_Morcillo_Caubera <amorcillo.severosom2020g@gmail.com>
2023-04-02 15:56:42 +0200

    Primera parte terminada

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 //Alex Morcillo Caubera
 public class TestCoche {
     //Alex Morcillo Caubera
     @Test
-    public void test_crear_coche() {
+    public void test_al_crear_un_coche_su_velocidad_es_cero() {
         Coche nuevoCoche = new Coche();
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
