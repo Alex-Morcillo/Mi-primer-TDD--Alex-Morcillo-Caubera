157d353083da38446b2f6247293bfd980668c65e
Alex_Morcillo_Caubera <amorcillo.severosom2020g@gmail.com>
2023-04-02 15:26:56 +0200

    Corregido fallo al crear repositorio

---
+++ tests/TestCoche.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+//Alex Morcillo Caubera
+public class TestCoche {
+    //Alex Morcillo Caubera
+    @Test
+    public void test_crear_coche() {
+        Coche nuevoCoche = new Coche();
+    }
+}
