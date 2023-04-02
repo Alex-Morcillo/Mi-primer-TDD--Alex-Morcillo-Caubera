627bb81734e2f1e0a904da32a64852e404948712
Alex_Morcillo_Caubera <amorcillo.severosom2020g@gmail.com>
2023-04-02 16:33:38 +0200

    Finalizada segunda parte

--- tests/Coche.java
+++ tests/Coche.java
@@ -1,3 +1,14 @@
 public class Coche {
     public int velocidad;
+
+    //Alex Morcillo Caubera
+    public void acelerar(int aceleracion) {
+        velocidad += aceleracion;
+    }
+
+    //Alex Morcillo Caubera
+    public void decelerar(int deceleracion) {
+        velocidad -= deceleracion;
+        if (velocidad < 0) velocidad = 0;
+    }
 }
